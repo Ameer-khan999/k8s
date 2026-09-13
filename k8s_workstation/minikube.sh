f00d719cb24129556e980a8ff559cfcbc4dae358
Ameer <your_email@example.com>
2026-09-13 13:55:00 +0530

    minikube.sh

---
+++ k8s_workstation/minikube.sh
@@ -0,0 +1,7 @@
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -r -l +100%FREE /dev/RootVG/varVol
+curl -sfL https://get.k3s.io | sh -
+export USER="ec2-user"
+mkdir ~/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chown $USER ~/.kube/config
