7e1dfd3a97002b0e005ede925d469e712943d81e
Ameer-khan999 <ameerkhanpathan999@gmail.com>
2026-09-26 05:53:44 +0000

    k8s

--- k8s_workstation/git.sh
+++ k8s_workstation/git.sh
@@ -1,4 +1,4 @@
 #git init
 git config --global user.email "ameerkhanpathan999@gmail.com"
 git config --global user.name "Ameer-khan999"
-git remote add origin https://github.com/Ameer-khan999/k8s
+#git remote add origin https://github.com/Ameer-khan999/k8s
